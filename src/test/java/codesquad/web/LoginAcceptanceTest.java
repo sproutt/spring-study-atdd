93fb14d0cd826ad1bdfb13c1dccf3e4bfd324518
wijehyeon <xile803@gmail.com>
2019-03-08 20:15:08 +0900

    feat : LoginAcceptanceTest login테스트 추가

--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -1,22 +1,43 @@
 package codesquad.web;
 
-		import codesquad.domain.UserRepository;
-		import org.junit.Test;
-		import org.springframework.beans.factory.annotation.Autowired;
-		import support.HtmlFormDataBuilder;
-		import support.test.AcceptanceTest;
+
+import codesquad.domain.User;
+import codesquad.domain.UserRepository;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import support.test.AcceptanceTest;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LoginAcceptanceTest extends AcceptanceTest {
+
+	private static final Logger log = LoggerFactory.getLogger(UserAcceptanceTest.class);
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Test
 	public void login() throws Exception {
-		HtmlFormDataBuilder.urlEncodedForm()
-				.addParameter("userid", "jehyeon")
-				.addParameter("passwrd", "1234")
-				.build();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("userId", "jehyeon");
+		params.add("password", "1234");
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+		ResponseEntity<String> response = template().postForEntity("/users/login", request, String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(userRepository.findByUserId("jehyeon").isPresent()).isTrue();
+		assertThat(response.getHeaders().getLocation().getPath()).startsWith("/users/login");
 	}
 }
