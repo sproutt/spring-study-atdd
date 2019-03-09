dd1100d4d1d01ca7f64136ad18c7d1a8f55ebb5b
Bellroute <kjkun7631@gmail.com>
2019-03-10 00:08:45 +0900

    feat: LoginAcceptanceTest 구현

---
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -0,0 +1,56 @@
+package codesquad.web;
+
+import codesquad.domain.User;
+import codesquad.domain.UserRepository;
+import codesquad.service.UserService;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import support.test.AcceptanceTest;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoginAcceptanceTest extends AcceptanceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void loginForm() throws Exception {
+        ResponseEntity<String> response = template().getForEntity("/login", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void login() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+
+        String userId = "testuser";
+        String password = "test";
+
+        userService.add(new User(userId, password, "bellroute", "bell@gmail.com"));
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("userId", userId);
+        params.add("password", password);
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = template().postForEntity("/users", request, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        assertThat(userRepository.findByUserId(userId).isPresent()).isTrue();
+        assertThat(userRepository.findByUserId(userId).orElseThrow(RuntimeException::new).getPassword()).isEqualTo(password);
+        assertThat(response.getHeaders().getLocation().getPath()).startsWith("/users");
+    }
+}
