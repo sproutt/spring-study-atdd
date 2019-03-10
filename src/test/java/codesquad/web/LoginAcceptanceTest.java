c42be36806d4546213de4182b3917b150eecb71b
ChangHwan-Lee <esp2ar0@gmail.com>
2019-03-10 16:56:22 +0900

    refactor: LoginAcceptanceTest

--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -13,22 +13,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LoginAcceptanceTest extends AcceptanceTest {
 
 	@Test
-	public void login() throws Exception {
+	public void login_success() throws Exception {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-		String userId = "testuser";
+		String userId = "javajigi";
 		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
 		params.add("userId", userId);
-		params.add("password", "password");
+		params.add("password", "test");
 		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
 
 		ResponseEntity<String> response = template().postForEntity("/users/login", request, String.class);
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
-		//assertThat(userRepository.findByUserId(userId).isPresent()).isTrue();
 		assertThat(response.getHeaders().getLocation().getPath()).startsWith("/users");
+	}
+
+	@Test
+	public void login_fail() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		String userId = "test";
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("userId", userId);
+		params.add("password", "test");
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+		ResponseEntity<String> response = template().postForEntity("/users/login", request, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
