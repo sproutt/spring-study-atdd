783f7297fb3b92026f6fb678eccb06595359b6c6
ChangHwan-Lee <esp2ar0@gmail.com>
2019-03-12 13:08:22 +0900

    feat: question-post 기능 컨트롤러에 구현

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,15 +1,30 @@
 package codesquad.web;
 
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
 
+	@Autowired
+	private QnaService qnaService;
+
 	@GetMapping("/form")
 	public String form() {
 		return "/qna/form";
+	}
+
+	@PostMapping("")
+	public String post(@LoginUser User loginUser, Question question) {
+		qnaService.create(loginUser, question);
+		return "redirect:/";
 	}
 }
