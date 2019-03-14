0295c9fbb7fdd64e749acd8c41ec9ead46f8eca5
SimKiSeong <totokisung@naver.com>
2019-03-14 12:41:18 +0900

    remove : unused import

--- src/main/java/codesquad/service/UserService.java
+++ src/main/java/codesquad/service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Optional;
 
 @Service("userService")
 public class UserService {
