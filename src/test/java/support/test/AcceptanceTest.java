0e14738965c2957e2769ca782044d15dbb2a1939
wijehyeon <xile803@gmail.com>
2019-03-14 17:06:17 +0900

    chore

--- src/test/java/support/test/AcceptanceTest.java
+++ src/test/java/support/test/AcceptanceTest.java
@@ -16,38 +16,38 @@ import java.util.NoSuchElementException;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AcceptanceTest {
-    private static final String DEFAULT_LOGIN_USER = "javajigi";
+	private static final String DEFAULT_LOGIN_USER = "javajigi";
 
-    @Autowired
-    private TestRestTemplate template;
+	@Autowired
+	private TestRestTemplate template;
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private QuestionRepository questionRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
 
-    public TestRestTemplate template() {
-        return template;
-    }
+	public TestRestTemplate template() {
+		return template;
+	}
 
-    public TestRestTemplate basicAuthTemplate() {
-        return basicAuthTemplate(defaultUser());
-    }
+	public TestRestTemplate basicAuthTemplate() {
+		return basicAuthTemplate(defaultUser());
+	}
 
-    public TestRestTemplate basicAuthTemplate(User loginUser) {
-        return template.withBasicAuth(loginUser.getUserId(), loginUser.getPassword());
-    }
+	public TestRestTemplate basicAuthTemplate(User loginUser) {
+		return template.withBasicAuth(loginUser.getUserId(), loginUser.getPassword());
+	}
 
-    protected User defaultUser() {
-        return findByUserId(DEFAULT_LOGIN_USER);
-    }
+	protected User defaultUser() {
+		return findByUserId(DEFAULT_LOGIN_USER);
+	}
 
-    protected User findByUserId(String userId) {
-        return userRepository.findByUserId(userId).get();
-    }
+	protected User findByUserId(String userId) {
+		return userRepository.findByUserId(userId).get();
+	}
 
-    protected Question defaultQuestion(){
-        return questionRepository.findById(defaultUser().getId()).orElseThrow(NoSuchElementException::new);
-    }
+	protected Question defaultQuestion() {
+		return questionRepository.findById(defaultUser().getId()).orElseThrow(NoSuchElementException::new);
+	}
 }
