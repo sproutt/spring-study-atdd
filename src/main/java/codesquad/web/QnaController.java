f1ace483afe182efe25ec2392a7161d5b1da316f
wijehyeon <xile803@gmail.com>
2019-03-19 23:36:54 +0900

    add : QnaService GetMappings

--- src/main/java/codesquad/web/QnaController.java
+++ src/main/java/codesquad/web/QnaController.java
@@ -5,6 +5,7 @@ import codesquad.domain.User;
 import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,9 +18,16 @@ public class QnaController {
 	@Resource(name = "qnaService")
 	private QnaService qnaService;
 
-	@GetMapping("")
-	public String list() {
-		return "redirect:/questions";
+	@GetMapping("/{id}")
+	public String list(@PathVariable long id, Model model) {
+		model.addAttribute("question", qnaService.findById(id));
+		return "/qna/show";
+	}
+
+	@GetMapping("/{id}/show")
+	public String updateForm(@PathVariable long id, Model model){
+		model.addAttribute("question", qnaService.findById(id));
+		return "/qna/updateForm";
 	}
 
 	@PostMapping("")
--- src/test/java/codesquad/service/QnaServiceTest.java
+++ src/test/java/codesquad/service/QnaServiceTest.java
@@ -1,9 +1,11 @@
 package codesquad.service;
 
+import codesquad.UnAuthorizedException;
 import codesquad.domain.Question;
 import codesquad.domain.QuestionRepository;
 import codesquad.domain.User;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -18,45 +20,50 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 public class QnaServiceTest {
 
-	@InjectMocks
-	private QnaService qnaService;
+    @InjectMocks
+    private QnaService qnaService;
 
-	@Mock
-	private QuestionRepository questionRepository;
+    @Mock
+    private QuestionRepository questionRepository;
 
-	private User user;
-	private Question question;
+    private User user;
+    private Question question;
 
-	@Before
-	public void setUp() {
-		user = new User("TestId", "password", "name", "email");
-		question = new Question("title", "contents");
-		questionRepository.save(question);
-	}
+    @Before
+    public void setUp() {
+        user = new User("TestId", "password", "name", "email");
+        question = new Question("title", "contents");
+        questionRepository.save(question);
+        question.setId(1L);
+        question.writeBy(user);
+    }
 
-	@Test
-	public void create() {
-		Question question = new Question();
-		when(questionRepository.save(question)).thenReturn(question);
-		assertThat(qnaService.create(user, question).getWriter().equals(user));
-	}
+    @Test
+    public void create() {
+        Question question = new Question();
+        when(questionRepository.save(question)).thenReturn(question);
+        assertThat(qnaService.create(user, question).getWriter().equals(user));
+    }
 
-	@Test
-	public void update() {
-		question.setId(1L);
-		question.writeBy(user);
-		when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
-		Question updatedQuestion = qnaService.update(user, 1L, question);
-		assertThat(qnaService.update(user, 1L, updatedQuestion).getTitle().equals("title"));
-		assertThat(qnaService.update(user, 1L, updatedQuestion).getContents().equals("contents"));
-	}
+    @Test
+    public void update() {
+        when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
+        Question updatedQuestion = qnaService.update(user, 1L, question);
+        assertThat(qnaService.update(user, 1L, updatedQuestion).getTitle().equals("title"));
+        assertThat(qnaService.update(user, 1L, updatedQuestion).getContents().equals("contents"));
+    }
 
-	@Test
-	public void delete() throws Exception {
-		question.setId(1L);
-		question.writeBy(user);
-		when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
-		qnaService.deleteQuestion(user, 1L);
-		assertThat(question.isDeleted()).isTrue();
-	}
+    @Test(expected = UnAuthorizedException.class)
+    public void update_failed() {
+        User anotherUser = new User("userId", "password", "anotherName", "anotherEmail");
+        when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
+        qnaService.update(anotherUser, 1L, question);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
+        qnaService.deleteQuestion(user, 1L);
+        assertThat(question.isDeleted()).isTrue();
+    }
 }
