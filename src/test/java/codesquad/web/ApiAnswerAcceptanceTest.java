250efcd32e4c734469d03e071e254a432327a84c
wijehyeon <xile803@gmail.com>
2019-03-23 20:31:16 +0900

    add : ApiAnswerAcceptanceTest

--- src/test/java/codesquad/web/ApiAnswerAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiAnswerAcceptanceTest.java
@@ -1,4 +1,49 @@
 package codesquad.web;
 
-public class ApiAnswerAcceptanceTest {
+import codesquad.domain.Answer;
+import org.junit.Before;
+import org.junit.Test;
+import support.test.AcceptanceTest;
+
+public class ApiAnswerAcceptanceTest extends AcceptanceTest {
+
+    private static final String ANSWER_API = "/api/answers";
+
+    private Answer answer;
+    private String resourceLocation;
+    
+    @Test
+    public void create() {
+        answer = getResource(resourceLocation)
+    }
+
+    @Test
+    public void create_failed() {
+
+    }
+
+    @Test
+    public void show(){
+
+    }
+
+    @Test
+    public void update() {
+
+    }
+
+    @Test
+    public void update_failed() {
+
+    }
+
+    @Test
+    public void delete() {
+
+    }
+
+    @Test
+    public void delete_failed() {
+
+    }
 }
