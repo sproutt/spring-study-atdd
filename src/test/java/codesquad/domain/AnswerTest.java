f9fdc07cda6a572be2169ff49fb95c2f1f49b5b1
dadadamarine <dadadamarine@gmail.com>
2019-03-24 00:30:45 +0900

    feature : Answer Validation 테스트 추가

---
+++ src/test/java/codesquad/domain/AnswerTest.java
@@ -0,0 +1,33 @@
+package codesquad.domain;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswerTest {
+    private static final Logger log = LoggerFactory.getLogger(AnswerTest.class);
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setup(){
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator= validatorFactory.getValidator();
+    }
+
+    @Test
+    public void empty_content() throws Exception{
+        Answer answer = new Answer();
+        Set<ConstraintViolation<Answer>> constraintViolations = validator.validate(answer);
+        assertThat(constraintViolations.size()).isEqualTo(1);
+    }
+}
