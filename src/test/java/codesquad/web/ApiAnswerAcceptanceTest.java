0fad7f9cea54491491a1494a6fa632571892c56f
ChangHwan-Lee <esp2ar0@gmail.com>
2019-03-24 18:28:18 +0900

    add: ApiAnswerAcceptanceTest - update test

--- src/test/java/codesquad/web/ApiAnswerAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiAnswerAcceptanceTest.java
@@ -4,10 +4,12 @@ import codesquad.domain.Answer;
 import codesquad.domain.Question;
 import codesquad.domain.User;
 import org.junit.Test;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import support.test.AcceptanceTest;
 
+import static codesquad.domain.UserTest.newUser;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApiAnswerAcceptanceTest extends AcceptanceTest {
@@ -49,5 +51,43 @@ public class ApiAnswerAcceptanceTest extends AcceptanceTest {
                 template().getForEntity(createResource("/api" + question.generateUrl() + "/answers", newAnswer, user), Void.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void update() {
+        User user = defaultUser();
+        Question question = defaultQuestion();
+        Answer newAnswer = new Answer();
+        newAnswer.setContents("contents4");
+
+        Answer updateAnswer = new Answer();
+        updateAnswer.setContents("updatecontents1");
+
+        ResponseEntity<Answer> responseEntity =
+                basicAuthTemplate(user)
+                        .exchange(createResource("/api" + question.generateUrl() + "/answers", newAnswer, user), HttpMethod.PUT,
+                                createHttpEntity(updateAnswer), Answer.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(updateAnswer.getContents()).isEqualTo(responseEntity.getBody().getContents());
+    }
+
+    @Test
+    public void update_다른_사람() {
+        User user = newUser("testuser1");
+        createResource("/api/users", user);
+        Question question = defaultQuestion();
+        Answer newAnswer = new Answer();
+        newAnswer.setContents("contents5");
+
+        Answer updateAnswer = new Answer();
+        updateAnswer.setContents("updatecontents2");
+
+        ResponseEntity<Answer> responseEntity =
+                basicAuthTemplate(user)
+                        .exchange(createResource("/api" + question.generateUrl() + "/answers", newAnswer, user), HttpMethod.PUT,
+                                createHttpEntity(updateAnswer), Answer.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
 }
