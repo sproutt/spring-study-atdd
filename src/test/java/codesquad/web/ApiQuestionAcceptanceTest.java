ab61895f657765b294baf427caf4b14900f481a6
ChangHwan-Lee <esp2ar0@gmail.com>
2019-03-24 15:22:31 +0900

    add: ApiQuestionAcceptanceTest - show Test

--- src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
@@ -21,4 +21,31 @@ public class ApiQuestionAcceptanceTest extends AcceptanceTest {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
+
+    @Test
+    public void post_no_login() {
+        User user = defaultUser();
+        Question newQuestion = new Question("title1", "contents1");
+        newQuestion.writeBy(user);
+
+        ResponseEntity<Void> response = template().postForEntity("/api/questions", newQuestion, Void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    public void show() {
+        User user = defaultUser();
+        Question newQuestion = new Question("title1", "contents1");
+        newQuestion.writeBy(user);
+
+        ResponseEntity<Void> response = basicAuthTemplate(user).postForEntity("/api/questions", newQuestion, Void.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        String location = response.getHeaders().getLocation().getPath();
+
+        response = template().getForEntity(location, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
