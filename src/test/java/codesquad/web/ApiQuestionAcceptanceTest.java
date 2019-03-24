58cd05115ac694f22ec949acfbeb8ddce956a3ef
SimKiSeong <totokisung@naver.com>
2019-03-24 15:37:03 +0900

    feat : ApiQuestionAcceptanceTest

---
+++ src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
@@ -0,0 +1,100 @@
+package codesquad.web;
+
+import codesquad.UnAuthenticationException;
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.domain.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import support.test.AcceptanceTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApiQuestionAcceptanceTest extends AcceptanceTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    User anotherUser;
+
+    @Before
+    public void setUp() throws UnAuthenticationException {
+        anotherUser = userRepository.findByUserId("sanjigi").orElseThrow(UnAuthenticationException::new);
+    }
+
+    @Test
+    public void create() throws Exception {
+        Question newQuestion = new Question("testQuestion1","testContents");
+
+        ResponseEntity<Void> response = basicAuthTemplate(defaultUser()).postForEntity("/api/quetions", newQuestion, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        Question dbQuestion = basicAuthTemplate(defaultUser()).getForObject(location, Question.class);
+        assertThat(dbQuestion).isNotNull();
+    }
+
+    @Test
+    public void update() throws Exception {
+
+        Question newQuestion = new Question("testQuestion2","testContents");
+        ResponseEntity<Void> response = basicAuthTemplate(defaultUser()).postForEntity("/api/quetions", newQuestion, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        Question updateQuestion = new Question("updateQuestion1","updateContents");
+        ResponseEntity<Void> responseEntity =
+                basicAuthTemplate(defaultUser()).exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), Void.class);
+        assertThat(updateQuestion.isEqualTitleAndContents(responseEntity.getBody())).isTrue();
+    }
+
+    @Test
+    public void update_다른_사람() throws Exception {
+        Question newQuestion = new Question("testQuestion3","testContents");
+        ResponseEntity<Void> response = basicAuthTemplate(defaultUser()).postForEntity("/api/quetions", newQuestion, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        Question updateQuestion = new Question("updateQuestion2","updateContents");
+
+        ResponseEntity<Void> responseEntity =
+                basicAuthTemplate(anotherUser).exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), Void.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        Question newQuestion = new Question("testQuestion3","testContents");
+        ResponseEntity<Void> response = basicAuthTemplate(defaultUser()).postForEntity("/api/quetions", newQuestion, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        basicAuthTemplate(defaultUser()).delete(location);
+
+        Question dbQuestion = template().getForObject(location, Question.class);
+        assertThat(dbQuestion.isDeleted()).isTrue();
+    }
+
+    @Test
+    public void delete_다른사람() throws Exception {
+        Question newQuestion = new Question("testQuestion4","testContents");
+        ResponseEntity<Void> response = basicAuthTemplate(defaultUser()).postForEntity("/api/quetions", newQuestion, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        basicAuthTemplate(anotherUser).delete(location);
+
+        Question dbQuestion = template().getForObject(location, Question.class);
+        assertThat(dbQuestion.isDeleted()).isFalse();
+    }
+
+
+
+    private HttpEntity createHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
+    }
+}
