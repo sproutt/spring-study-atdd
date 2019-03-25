61629e05d1a633667add50fb07c94883822ed84c
wijehyeon <xile803@gmail.com>
2019-03-25 10:59:29 +0900

    add : ApiAnswerController - create()

--- src/main/java/codesquad/web/ApiAnswerController.java
+++ src/main/java/codesquad/web/ApiAnswerController.java
@@ -1,25 +1,32 @@
 package codesquad.web;
 
+import codesquad.domain.Answer;
 import codesquad.domain.User;
 import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/questions/{questionId}/answers")
 public class ApiAnswerController {
-    @Resource(name="qnaService")
+    @Resource(name = "qnaService")
     private QnaService qnaService;
 
     @PostMapping
-    public ResponseEntity<Void> create(@LoginUser User loginUser, @PathVariable long id){
-
+    public ResponseEntity<Void> create(@LoginUser User loginUser, @PathVariable long questionId, @Valid @RequestBody Answer answer) throws Exception {
+        Answer savedAnswer = qnaService.addAnswer(loginUser, questionId, answer.getContents());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/api/questions/" + questionId + "/answers" + savedAnswer.getId()));
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
+
+
 }
 
