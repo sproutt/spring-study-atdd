23839a8cc1aec106f9fe3703d5f24b6c452f0db9
kanamycine <jinyeong5320@gmail.com>
2022-05-01 13:07:25 +0900

    feat : 로그인 성공/실패 테스트 구현

--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -39,7 +39,46 @@ public class LoginAcceptanceTest extends AcceptanceTest{
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
 		assertThat(userRepository.findByUserId(userId).isPresent()).isTrue();
-		assertThat(response.getHeaders().getLocation().getPath()).startsWith("/");
+		assertThat(response.getHeaders().getLocation().getPath()).startsWith("/users");
+	}
+
+	@Test
+	public void login_failed_mismatch_password(){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		String userId = "javajigi";
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("userId", userId);
+		params.add("password", "1234");
+
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+		ResponseEntity<String> response = template().postForEntity("/users/login", request, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(userRepository.findByUserId(userId).isPresent()).isTrue();
+	}
+
+	@Test
+	public void login_failed_mismatch_id(){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		String userId = "javajig";
+		String password = "12345";
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		params.add("userId", userId);
+		params.add("password", password);
+
+		HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+		ResponseEntity<String> response = template().postForEntity("/users/login", request, String.class);
+
+		assertThat(userRepository.findByUserId(userId).isPresent()).isFalse();
+		assertThat(HttpStatus.UNAUTHORIZED.equals(response.getStatusCode()));
 	}
 
 }
