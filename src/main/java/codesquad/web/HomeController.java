7ba01cb2dccdd6879651109c091a51fde6256f05
kanamycine <jinyeong5320@gmail.com>
2022-05-06 22:43:57 +0900

    feat : home questions list 기능 구현

--- src/main/java/codesquad/web/HomeController.java
+++ src/main/java/codesquad/web/HomeController.java
@@ -1,13 +1,22 @@
 package codesquad.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import codesquad.service.QuestionService;
+
 @Controller
 public class HomeController {
+
+    @Resource(name = "questionService")
+    private QuestionService questionService;
+
     @GetMapping("/")
     public String home(Model model) {
+        model.addAttribute("questions", questionService.findAll());
         return "home";
     }
 }
