69b9f1d5c015ab197eaa4f06a7a9633f508182e6
user <gusdn85554@naver.com>
2022-05-06 23:12:56 +0900

    refactor : Helper 클래스를 사용하여 중복된 코드 제거

--- src/test/java/codesquad/web/LoginAcceptanceTest.java
+++ src/test/java/codesquad/web/LoginAcceptanceTest.java
@@ -4,11 +4,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import support.helper.HtmlFormDataBuilder;
 import support.test.AcceptanceTest;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,19 +17,14 @@ public class LoginAcceptanceTest extends AcceptanceTest {
     @Test
     public void login_success() throws Exception {
         //given
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-        params.add("userId", defaultUser().getUserId());
-        params.add("password", defaultUser().getPassword());
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
-
-        log.debug("request = {}", request);
+        HtmlFormDataBuilder htmlFormDataBuilder = HtmlFormDataBuilder.urlEncodedForm();
+        htmlFormDataBuilder.addParameter("userId", defaultUser().getUserId());
+        htmlFormDataBuilder.addParameter("password", defaultUser().getPassword());
 
         //when
-        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/users/login", request, String.class);
+        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/users/login", htmlFormDataBuilder.build(), String.class);
+
+        log.info("response = {}", response);
 
         //then
         assertAll(
@@ -44,19 +36,12 @@ public class LoginAcceptanceTest extends AcceptanceTest {
     @Test
     public void login_fail() throws Exception{
         //given
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-        params.add("userId", defaultUser().getUserId());
-        params.add("password", defaultUser().getPassword() + "1");
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
-
-        log.debug("request = {}", request);
+        HtmlFormDataBuilder htmlFormDataBuilder = HtmlFormDataBuilder.urlEncodedForm();
+        htmlFormDataBuilder.addParameter("userId", defaultUser().getUserId());
+        htmlFormDataBuilder.addParameter("password", defaultUser().getPassword() + "1");
 
         //when
-        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/users/login", request, String.class);
+        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/users/login", htmlFormDataBuilder.build(), String.class);
 
         log.info("response = {}", response);
 
