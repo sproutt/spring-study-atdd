f51a3464bd28773f64dee537b652fb6ec4b10c51
sunghyuki <sunghyuk1609@gmail.com>
2022-05-08 16:36:41 +0900

    feat : 질문 생성 기능 컨트롤러 메서드 추가

--- src/main/java/codesquad/web/QuestionController.java
+++ src/main/java/codesquad/web/QuestionController.java
@@ -1,20 +1,34 @@
 package codesquad.web;
 
+import codesquad.domain.Question;
 import codesquad.domain.User;
 import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
     private static final Logger log = LoggerFactory.getLogger(QuestionController.class);
 
+    @Resource(name = "qnaService")
+    private QnaService qnaService;
+
     @GetMapping("/form")
     public String questionForm(@LoginUser User loginUser) {
         return "/qna/form";
+    }
+
+    @PostMapping("")
+    public String create(@LoginUser User loginUser, Question question) {
+        qnaService.create(loginUser, question);
+        return "redirect:/";
     }
 }
