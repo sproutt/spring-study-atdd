2e7a493f6f5191cf294ea948593e22057729a927
sunghyuki <sunghyuk1609@gmail.com>
2022-05-12 00:29:31 +0900

    test : 질문 삭제 기능 단위 테스트 추가 - 삭제 성공 단위 테스트 추가 - 질문 작성자가 일치하지 않을 경우 단위 테스트 추가 - 질문 데이터가 존재하지 않을 경우 단위 테스트 추가

--- src/test/java/codesquad/service/QnaServiceTest.java
+++ src/test/java/codesquad/service/QnaServiceTest.java
@@ -1,5 +1,6 @@
 package codesquad.service;
 
+import codesquad.CannotDeleteException;
 import codesquad.UnAuthorizedException;
 import codesquad.domain.*;
 import org.junit.Test;
@@ -65,7 +66,7 @@ public class QnaServiceTest {
     public void update_question_success() {
         User loginUser = UserTest.newUser(USER_ID);
         Question question = QuestionTest.newQuestion(QUESTION_ID, loginUser);
-        Question targetQuestion = QuestionTest.updatedQuestion("오늘의 할 일은?", "자동차 주차하기", loginUser);
+        Question targetQuestion = QuestionTest.updatedQuestion(QUESTION_ID, "오늘의 할 일은?", "자동차 주차하기", loginUser);
         when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.of(question));
 
         Question updatedQuestion = qnaService.update(loginUser, QUESTION_ID, targetQuestion);
@@ -80,7 +81,7 @@ public class QnaServiceTest {
         User user = UserTest.newUser(USER_ID);
         User loginUser = UserTest.SANJIGI;
         Question question = QuestionTest.newQuestion(QUESTION_ID, user);
-        Question targetQuestion = QuestionTest.updatedQuestion("오늘의 할 일은?", "자동차 주차하기", loginUser);
+        Question targetQuestion = QuestionTest.updatedQuestion(QUESTION_ID, "오늘의 할 일은?", "자동차 주차하기", loginUser);
         when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.of(question));
 
         qnaService.update(loginUser, QUESTION_ID, targetQuestion);
@@ -89,9 +90,38 @@ public class QnaServiceTest {
     @Test(expected = EntityNotFoundException.class)
     public void update_failed_when_entity_not_found() {
         User loginUser = UserTest.newUser(USER_ID);
-        Question targetQuestion = QuestionTest.updatedQuestion("오늘의 할 일은?", "자동차 주차하기", loginUser);
+        Question targetQuestion = QuestionTest.updatedQuestion(QUESTION_ID, "오늘의 할 일은?", "자동차 주차하기", loginUser);
         when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.empty());
 
         qnaService.update(loginUser, QUESTION_ID, targetQuestion);
+    }
+
+    @Test
+    public void delete_success() throws Exception {
+        User loginUser = UserTest.newUser(USER_ID);
+        Question question = QuestionTest.newQuestion(QUESTION_ID, loginUser);
+        when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.of(question));
+
+        qnaService.deleteQuestion(loginUser, question.getId());
+
+        assertThat(question.isDeleted()).isTrue();
+    }
+
+    @Test(expected = CannotDeleteException.class)
+    public void delete_failed_when_writer_mismatch() throws Exception {
+        User user = UserTest.newUser(USER_ID);
+        User loginUser = UserTest.SANJIGI;
+        Question question = QuestionTest.newQuestion(QUESTION_ID, user);
+        when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.of(question));
+
+        qnaService.deleteQuestion(loginUser, question.getId());
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void delete_failed_when_entity_not_found() throws Exception {
+        User loginUser = UserTest.newUser(USER_ID);
+        when(questionRepository.findById(QUESTION_ID)).thenReturn(Optional.empty());
+
+        qnaService.deleteQuestion(loginUser, QUESTION_ID);
     }
 }
