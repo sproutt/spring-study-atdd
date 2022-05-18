af5e7c1a2c00afa3ac58bf3bc706b2cd966a5f46
user <gusdn85554@naver.com>
2022-05-18 16:16:17 +0900

    refactor : [리뷰 반영] intellij wildcard 수정

--- src/main/java/codesquad/web/dto/QuestionDto.java
+++ src/main/java/codesquad/web/dto/QuestionDto.java
@@ -1,6 +1,11 @@
 package codesquad.web.dto;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Objects;
 
