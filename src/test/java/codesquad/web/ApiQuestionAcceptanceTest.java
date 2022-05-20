24fd898f8d75678bc7f15b24f51227b72ca5b4b6
saint6839 <saint6839@gmail.com>
2022-05-20 22:52:00 +0900

    feat: [상엽] 질문 수정 인수테스트 구현
    -update_authorized() : 로그인 했을때 질문 수정 요청을 보내고, 질문이 잘 수정 되었는지 테스트

--- src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
+++ src/test/java/codesquad/web/ApiQuestionAcceptanceTest.java
@@ -1,9 +1,13 @@
 package codesquad.web;
 
+import codesquad.HtmlFormDataBuilder;
 import codesquad.domain.Question;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
 import static org.assertj.core.api.Assertions.*;
@@ -40,6 +44,21 @@ public class ApiQuestionAcceptanceTest extends AcceptanceTest {
         //then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).contains("국내에서 Ruby on Rails와 Play가 활성화되기 힘든 이유는 뭘까?");
+    }
+
+    @Test
+    public void update_authorized() {
+        //given
+        Question question = findByQuestionId(1L);
+        HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodeForm()
+                                                                             .addParameter("title", "title 수정")
+                                                                             .addParameter("contents", "contents 수정")
+                                                                             .build();
+        //when
+        ResponseEntity<String> response = basicAuthTemplate().exchange("/api/questions/1", HttpMethod.PUT, request, String.class);
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        assertThat(question.getTitle()).isEqualTo("title 수정");
     }
 
     public Question createQuestion() {
