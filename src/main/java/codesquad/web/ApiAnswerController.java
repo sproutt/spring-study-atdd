7c6165091e74a2576a5e56cf210a22e6c9714e2e
saint6839 <saint6839@gmail.com>
2022-05-22 15:59:40 +0900

    feat: [상엽] 답변 컨트롤러 답변 생성 메서드 구현

--- src/main/java/codesquad/web/ApiAnswerController.java
+++ src/main/java/codesquad/web/ApiAnswerController.java
@@ -1,12 +1,17 @@
 package codesquad.web;
 
+import codesquad.domain.Answer;
+import codesquad.domain.User;
+import codesquad.security.LoginUser;
 import codesquad.service.QnaService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/questions")
 public class ApiAnswerController {
+    public static final Logger log = LoggerFactory.getLogger(ApiAnswerController.class);
 
     private final QnaService qnaService;
 
@@ -14,4 +19,12 @@ public class ApiAnswerController {
         this.qnaService = qnaService;
     }
 
+    @PostMapping("/{question_id}/answers")
+    public Answer create(@LoginUser User user, @PathVariable("question_id") long questionId, @RequestBody String contents) {
+        log.info("user = {}", user);
+        log.info("question-id = {}", questionId);
+        log.info("contents = {}", contents);
+
+        return qnaService.addAnswer(user, questionId, contents);
+    }
 }
