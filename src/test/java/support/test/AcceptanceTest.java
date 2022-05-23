882bef1a5f30971269afb3d415b3037b69f96dc3
sunghyuki <sunghyuk1609@gmail.com>
2022-05-23 19:06:24 +0900

    test : 답변 수정 API 성공, 실패 인수 테스트 추가 - createHttpEntity 메서드 중복 제거를 위해 AcceptanceTest 클래스로 이동

--- src/test/java/support/test/AcceptanceTest.java
+++ src/test/java/support/test/AcceptanceTest.java
@@ -9,8 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.Payload;
@@ -61,12 +60,18 @@ public abstract class AcceptanceTest {
     }
 
     protected String createResource(String path, Object bodyPayload) {
-        ResponseEntity<Question> response = basicAuthTemplate().postForEntity(path, bodyPayload, Question.class);
+        ResponseEntity<String> response = basicAuthTemplate().postForEntity(path, bodyPayload, String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         return response.getHeaders().getLocation().getPath();
     }
 
     protected <T> T getResource(String location, Class<T> responseType) {
         return basicAuthTemplate().getForObject(location, responseType);
+    }
+
+    protected HttpEntity createHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
     }
 }
