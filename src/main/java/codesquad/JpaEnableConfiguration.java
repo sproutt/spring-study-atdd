97cd575bcdac153c20971dfa5a3b534a2c6942a3
sunghyuki <sunghyuk1609@gmail.com>
2022-05-23 22:20:00 +0900

    feat : 컴포넌트 스캔 분리

---
+++ src/main/java/codesquad/JpaEnableConfiguration.java
@@ -0,0 +1,11 @@
+package codesquad;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+@ComponentScan({"codesquad", "support"})
+public class JpaEnableConfiguration {
+}
