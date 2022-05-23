57d3a6fc0e5ab181dfb8d1eb176fe9fe57f523ac
sunghyuki <sunghyuk1609@gmail.com>
2022-05-23 18:39:11 +0900

    feat : 답변 생성 API 컨트롤러 메서드 추가 - ApiAnswerController 클래스 생성

---
+++ src/main/java/codesquad/web/ApiAnswerController.java
@@ -0,0 +1,36 @@
+package codesquad.web;
+
+import codesquad.domain.Answer;
+import codesquad.domain.User;
+import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/questions/{questionId}/answers")
+public class ApiAnswerController {
+    private static final Logger log = LoggerFactory.getLogger(ApiAnswerController.class);
+
+    private final QnaService qnaService;
+
+    public ApiAnswerController(QnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Answer> create(@LoginUser User loginUser, @PathVariable Long questionId, @Valid @RequestBody String contents) {
+        Answer savedAnswer = qnaService.addAnswer(loginUser, questionId, contents);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/api/questions/" + questionId + "/answers/" + savedAnswer.getId()));
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+}
