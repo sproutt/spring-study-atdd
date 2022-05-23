1e2ead8f1c6e4d329130b7eedc65a5914e938574
sunghyuki <sunghyuk1609@gmail.com>
2022-05-23 14:50:12 +0900

    feat : 질문 생성 API 컨트롤러 메서드 추가 - ApiQuestionController 클래스 생성

---
+++ src/main/java/codesquad/web/ApiQuestionController.java
@@ -0,0 +1,41 @@
+package codesquad.web;
+
+import codesquad.domain.Question;
+import codesquad.domain.User;
+import codesquad.security.LoginUser;
+import codesquad.service.QnaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/questions")
+public class ApiQuestionController {
+    private static final Logger log = LoggerFactory.getLogger(ApiQuestionController.class);
+
+    private final QnaService qnaService;
+
+    public ApiQuestionController(QnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Question> create(@LoginUser User loginUser, @Valid @RequestBody Question question) {
+        Question savedQuestion = qnaService.create(loginUser, question);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/api/questions/" + savedQuestion.getId()));
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+}
