fa7f93381ded38890181b6587b3e276c210c3de6
saint6839 <saint6839@gmail.com>
2022-05-29 11:09:52 +0900

    feat: [상엽] CannotDeleteException 기본 생성자 추가

--- src/main/java/codesquad/CannotDeleteException.java
+++ src/main/java/codesquad/CannotDeleteException.java
@@ -3,6 +3,9 @@ package codesquad;
 public class CannotDeleteException extends Exception {
     private static final long serialVersionUID = 1L;
 
+    public CannotDeleteException() {
+    }
+
     public CannotDeleteException(String message) {
         super(message);
     }
